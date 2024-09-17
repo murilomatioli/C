b8bc7ba26295e52479c091c961c67747659408c2
Murilo Lima Matioli <96125707+murilomatioli@users.noreply.github.com>
2024-09-17 11:24:34 -0300

    Add files via upload

--- ponteiros/ponteiros6.c
+++ ponteiros/ponteiros6.c
@@ -32,37 +32,70 @@ typedef struct
     contaCorrente contas[5];
 } banco;
 
-void cadastrarCliente(banco *clientes){
-    clientes->id = 1;
-    clientes->idade = 20;
-    strcpy(clientes->nome, "murilo");
-    strcpy(clientes->endereco, "Rua Euclides da Cunha 294, Nova Ilha");
-    clientes->contas[0].id = 1;
-    clientes->contas[0].tempo = 5;
-    clientes->contas[0].saldo = 633.50;
-    clientes->contas[0].idConta = 1;
+void cadastrarCliente(banco *clientes,int *PnumClientes){
+    char nome[50], endereco[100];
+    int idade;
+    *PnumClientes +=1;
+    int index = *PnumClientes - 1;
+    clientes[index].id = *PnumClientes;
+    printf("Informe a idade do cliente: ");
+    scanf("%d", &idade);
+    clientes[index].idade = idade;
+    printf("Informe o nome do cliente: ");
+    scanf(" %[^\n]", nome);
+    printf("Informe o endereço do cliente: ");
+    scanf(" %[^\n]", endereco);
+    strcpy(clientes[index].nome, nome);
+    strcpy(clientes[index].endereco, endereco);
+    clientes[index].contas[0].id = 1;
+    clientes[index].contas[0].tempo = 5;
+    clientes[index].contas[0].saldo = 633.50;
+    clientes[index].contas[0].idConta = 1;
+
 }
-void exibirCliente(banco *clientes) {
+void exibirCliente(banco *clientes, int *PnumClientes) {
+    int localId = *PnumClientes - 1;
     // Exibir informações do cliente
-    printf("Id cliente: %d\n", clientes->id);
-    printf("Nome cliente: %s\n", clientes->nome);
-    printf("Idade cliente: %d\n", clientes->idade);
-    printf("Endereco cliente: %s\n", clientes->endereco);
+    banco localClientes = clientes[localId];
+
+
+    printf("Id cliente: %d\n", localClientes.id);
+    printf("Nome cliente: %s\n", localClientes.nome);
+    printf("Idade cliente: %d\n", localClientes.idade);
+    printf("Endereco cliente: %s\n", localClientes.endereco);
 
     // Exibir informações da primeira conta associada ao cliente
     printf("\n--- Conta 1 ---\n");
-    printf("Id da conta: %d\n", clientes->contas[0].id);
-    printf("Tempo de existência: %d meses\n", clientes->contas[0].tempo);
-    printf("Saldo da conta: %.2f\n", clientes->contas[0].saldo);
-    printf("Id da conta corrente: %d\n", clientes->contas[0].idConta);
+    printf("Id da conta: %d\n", localClientes.contas[0].id);
+    printf("Tempo de existência: %d meses\n", localClientes.contas[0].tempo);
+    printf("Saldo da conta: %.2f\n", localClientes.contas[0].saldo);
+    printf("Id da conta corrente: %d\n", localClientes.contas[0].idConta);
+
 }
 
 int main()
 {
+    int numClientes = 0;
+    char op[2];
+    int *PnumClientes = &numClientes;
     banco clientes[1000];
     banco *Pclientes = &clientes[0];
-    cadastrarCliente(Pclientes);
-    exibirCliente(Pclientes);
+    int encerrar = 0;
+    // 
+    while(encerrar != -1){
+        scanf("%s", op);
+        if(strcmp("cadastrar", op) == 0){
+            cadastrarCliente(Pclientes, PnumClientes);
+        }
+        if(strcmp("exibir", op) == 0){
+            exibirCliente(Pclientes, PnumClientes);
+        }
+        if(strcmp("encerrar", op) == 0){
+            encerrar = -1;
+        }
+    }
+    printf("Numero de clientes: %d\n", *PnumClientes);
+    
     return 0;
 }
 
